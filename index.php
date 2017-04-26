492d54c147890a6489cc36e9d81bf287efbb84b7
Dimitri <dimi_didou@hotmail.fr>
2017-04-26 21:33:12 +0200

    update index

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<script src="ressources/js/utility.js"></script>
+</head>
+<body>
+	<div id="div-intro">
+		<video id="intro" autoplay loop>
+			<source src="ressources/introtest.mp4" type="video/mp4">
+		</video>
+	</div>
+</body>
+</html>
