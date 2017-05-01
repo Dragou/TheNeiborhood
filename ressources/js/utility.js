0319802f6734376da3a9c77fe1fef4ece8c30d7e
Dimitri <dimi_didou@hotmail.fr>
2017-05-01 10:55:51 +0200

    Video final

--- ressources/js/utility.js
+++ ressources/js/utility.js
@@ -1,4 +1,4 @@
-function windowOneOn(player) {
+function windowOn(player) {
 	actuPlayer = document.getElementById(player);
 	if(actuPlayer.muted){
 		actuPlayer.muted = false;
@@ -7,47 +7,21 @@ function windowOneOn(player) {
 		actuPlayer.volume = 0.0;
 	}
 
-	for (var i = 0; i < 19; i++) {
-		actuPlayer.volume += 0.05;
-		sleep(100);
+	for (var i = 0; i < 10; i++) {
+		actuPlayer.volume += 0.1;
+		sleep(10);
 	}
-	console.log(player + ' : ' + actuPlayer.volume);
+	//console.log(player + ' : ' + actuPlayer.volume);
 }
 
-function windowOneOff(player) {
+function windowOff(player) {
 	actuPlayer = document.getElementById(player);
 
-	for (var i = 0; i < 19; i++) {
-		actuPlayer.volume -= 0.05;
-		sleep(100);
+	for (var i = 0; i < 10; i++) {
+		actuPlayer.volume -= 0.1;
+		sleep(10);
 	}
-	console.log(player + ' : ' + actuPlayer.volume);
-}
-
-function windowTwoOn(player) {
-	actuPlayer = document.getElementById(player);
-	if(actuPlayer.muted){
-		actuPlayer.muted = false;
-	}
-	if(actuPlayer.volume == 1){
-		actuPlayer.volume = 0.0;
-	}
-
-	for (var i = 0; i < 19; i++) {
-		actuPlayer.volume += 0.05;
-		sleep(100);
-	}
-	console.log(player + ' : ' + actuPlayer.volume);
-}
-
-function windowTwoOff(player) {
-	actuPlayer = document.getElementById(player);
-
-	for (var i = 0; i < 19; i++) {
-		actuPlayer.volume -= 0.05;
-		sleep(100);
-	}
-	console.log(player + ' : ' + actuPlayer.volume);
+	//console.log(player + ' : ' + actuPlayer.volume);
 }
 
 function sleep(milliseconds) {
